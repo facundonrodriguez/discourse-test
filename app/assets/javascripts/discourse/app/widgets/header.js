c77ca217641fcd441a61b8dae689080cf2ebfa87
Facundo Rodriguez <facundo.rodriguez@mdevz.com>
2023-04-04 17:29:23 -0300

    feat: remove sign in buttons

--- app/assets/javascripts/discourse/app/widgets/header.js
+++ app/assets/javascripts/discourse/app/widgets/header.js
@@ -367,15 +367,15 @@ createWidget("header-buttons", {
 
     const buttons = [];
 
-    if (attrs.canSignUp && !attrs.topic) {
-      buttons.push(
-        this.attach("button", {
-          label: "sign_up",
-          className: "btn-primary btn-small sign-up-button",
-          action: "showCreateAccount",
-        })
-      );
-    }
+    // if (attrs.canSignUp && !attrs.topic) {
+    //   buttons.push(
+    //     this.attach("button", {
+    //       label: "sign_up",
+    //       className: "btn-primary btn-small sign-up-button",
+    //       action: "showCreateAccount",
+    //     })
+    //   );
+    // }
 
     buttons.push(
       this.attach("button", {
